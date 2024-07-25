9b1f4ca3a3c4dfdde5a87beeb60c7a731aa8263f
Kakashi2025 <harshitsinghrawat5@gmail.com>
2024-07-25 12:15:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿public class A
+{
+    //Private members are visible only in derived classes
+    //that are nested in their base class. Otherwise,
+    //they are not visible in derived classes.
+    private int _value = 10;
+
+    public class B : A
+    {
+        public int GetValue()
+        {
+            return _value;
+        }
+    }
+}
+
+public class C : A
+{
+    // if you remove the comments from the C.GetValue method and attempt to compile the example,
+    // it produces compiler error CS0122: "'A._value' is inaccessible due to its protection level."
+    //    public int GetValue()
+    //    {
+    //        return _value;
+    //    }
+}
+
+public class AccessExample
+{
+    public static void Main(string[] args)
+    {
+        var b = new A.B();
+        Console.WriteLine(b.GetValue());
+    }
+}
+// The example displays the following output:
+//       10
